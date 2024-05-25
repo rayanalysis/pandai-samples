7696820a4aeb07fe17c3fef9945271801d424f49
rayanalysis <ray.analysis@gmail.com>
2024-05-25 15:12:12 -0400

    Adding stairs_and_terrain.py initial example program.

--- EggPrimitiveCreation.py
+++ EggPrimitiveCreation.py
@@ -1,8 +1,55 @@
 from panda3d.core import Point3D, deg2Rad, NodePath, Filename, CSZupRight
+from panda3d.core import CollisionNode,CollisionPolygon, GeomVertexFormat, Point3
 from panda3d.egg import EggPolygon, EggGroup, EggVertexPool, EggData, EggVertex, loadEggData, EggCoordinateSystem
 import math
 
 
+def makeCollisionModel(inputModel = "models/arena_1.bam", modelOffset = 0):
+    # we can utilize a collision mesh generated directly for the built-in collision system
+    # to obtain a varying heightfield, adapted from:
+    # https://discourse.panda3d.org/t/collision-mesh-from-loaded-model-for-built-in-collision-system/27102
+    # load model
+    path_to_model = inputModel
+    model_root = base.loader.loadModel(path_to_model)
+    model_root.reparentTo(base.render)
+
+    # create a temporary copy to generate the collision meshes from
+    model_copy = model_root.copyTo(base.render)
+    model_copy.detachNode()
+    # "bake" the transformations into the vertices
+    model_copy.flattenLight()
+
+    # create root node to attach collision nodes to
+    collision_root = NodePath("collision_root")
+    collision_root.reparentTo(model_root)
+    # offset the collision meshes from the model so they're easier to see
+    collision_root.setX(modelOffset)
+    collision_root.setY(modelOffset)
+
+    # create a collision mesh for each of the loaded models
+    for model in model_copy.findAllMatches("**/+GeomNode"):
+        model_node = model.node()
+        collision_node = CollisionNode(model_node.name)
+        collision_mesh = collision_root.attachNewNode(collision_node)
+        # collision nodes are hidden by default
+        collision_mesh.show()
+
+        for geom in model_node.modifyGeoms():
+
+            geom.decomposeInPlace()
+            vertex_data = geom.modifyVertexData()
+            vertex_data.format = GeomVertexFormat.get_v3()
+            view = memoryview(vertex_data.arrays[0]).cast("B").cast("f")
+            index_list = geom.primitives[0].getVertexList()
+            index_count = len(index_list)
+
+            for indices in (index_list[i:i+3] for i in range(0, index_count, 3)):
+                points = [Point3(*view[index*3:index*3+3]) for index in indices]
+                coll_poly = CollisionPolygon(*points)
+                collision_node.addSolid(coll_poly)
+
+    # model_root.hide()
+            
 def makeSquares(gridX = 30, gridY = 30, scale = 1, evpName = 'square', startPos = Point3D(0, 0, 0)):
     z_up = EggCoordinateSystem()
     z_up.setValue(CSZupRight)
